00995f292e3b3f49006b42c67c3baccd3a1a4ded
Yuri Lima <yurilimaprofissional@gmail.com>
2025-02-26 16:55:24 -0300

    ex010

---
+++ exercícios/ex009.js
@@ -0,0 +1,6 @@
+var país = 'índia'
+console.log(`Seu país é ${país} então `)
+if (país == 'Brasil')
+    {console.log(`você é Brasileiro!`)}
+else
+    {console.log(`você é estrageiro!`)}
---
+++ rascunho.js
@@ -0,0 +1,5 @@
+var n1 = 5
+if (n1 == 4)
+    {console.log(`O número ${n1} é igual a 4 !`)}
+else
+    {console.log(`O número ${n1} não é igual a 4.`)}

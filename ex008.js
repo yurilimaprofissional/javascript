eb59c143ac5cf8079d6cf9858b5427ad275b8118
Yuri Lima <yurilimaprofissional@gmail.com>
2025-02-26 15:42:18 -0300

    ex008js

---
+++ ex008.js
@@ -0,0 +1,7 @@
+var vel = 34
+console.log (`Sua velocidade é de ${vel}km/h`)
+if (vel > 60)
+     {console.log (`Você ultrapassou o limite de velocidade e por isso foi MULTADO!`)}
+else 
+    {console.log(`Parabéns! Você estar dirigindo com segurança.`)}
+console.log (`Dirija sempre usando o cinto de segurança.`)

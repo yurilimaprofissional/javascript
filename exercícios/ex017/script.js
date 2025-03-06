fbb67b8406cb0cea7da60e162d0240b1fe249d3e
Yuri Lima <yurilimaprofissional@gmail.com>
2025-03-06 19:02:53 -0300

    ex017

---
+++ exercícios/ex017/script.js
@@ -0,0 +1,9 @@
+function tabuada() {
+    var num = document.getElementById('num')
+    var tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor digite um número!')
+    }
+    var n = Number(num.value)
+
+}

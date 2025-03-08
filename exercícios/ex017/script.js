04643b995862f2e0c19782ac5233e313ca993ae4
Yuri Lima <yurilimaprofissional@gmail.com>
2025-03-07 22:52:11 -0300

    tabuada

--- exercícios/ex017/script.js
+++ exercícios/ex017/script.js
@@ -5,5 +5,11 @@ function tabuada() {
         window.alert('Por favor digite um número!')
     }
     var n = Number(num.value)
+    tab.innerHTML=''
+    for(var c = 1 ;c <= 10 ; c++ ) {
+        var item = document.createElement('option')
+        item.text = `${n} x ${c} = ${n*c}`
+        tab.appendChild(item)
+    }
 
 }

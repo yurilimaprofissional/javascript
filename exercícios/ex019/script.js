d03846a62dd48dff21d4180693bf12a82aabcf74
Yuri Lima <yurilimaprofissional@gmail.com>
2025-03-11 17:57:18 -0300

    ex019

--- exercícios/ex019/script.js
+++ exercícios/ex019/script.js
@@ -1,4 +1,4 @@
-var num = document.getElementById('add')
+var num = document.querySelector('input#num')
 var lista = document.querySelector('select#lista')
 var res = document.querySelector('div#res')
 var valores = []
@@ -14,7 +14,7 @@ function isNumero(n) {
 }
 
 function inLista(n, l) {
-    if (l.index(Number(n)) != -1) {
+    if (l.indexOf(Number(n)) != -1) {
         return true
 }
 
@@ -26,9 +26,46 @@ function inLista(n, l) {
 
 function adicionar() {
     if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
 
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
+    num.value=''
+    num.focus()
 
+}
+
+function analizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione algum valor antes de pedir para analizar.')
+    }
+
+    else {
+        var tot = valores.length
+        var maior = valores[0]
+        var menor = valores[0]
+        var soma = 0
+        var media = 0
+        for (var pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior) 
+                maior = valores[pos]
+        
+            if (valores[pos] < menor) 
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi o ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi o ${menor}.</p>` 
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores é ${media}</p>`
+
+    }
 }
